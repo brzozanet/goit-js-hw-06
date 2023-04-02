7672fbe5f9183d04af0431d50aa5a4c80b5ae3cf
Paweł Brzozowski <pawel@brzoza.net>
2023-04-02 19:33:34 +0200

    Task #02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.getElementById("ingredients");
+
+for (let ingredient of ingredients) {
+  let itemEL = document.createElement("li");
+  itemEL.innerHTML = ingredient;
+  itemEL.classList.add("item");
+  ingredientsEl.append(itemEL);
+};
