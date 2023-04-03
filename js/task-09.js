d58b19358fa30b1834c152cdabafbc4802f7b6a1
Paweł Brzozowski <pawel@brzoza.net>
2023-04-03 11:40:11 +0200

    Task #09

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,15 @@
+const textEl = document.querySelector(".color");
+const buttonEl = document.querySelector(".change-color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+function changeColor(event) {
+  textEl.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
+};
+
+buttonEl.addEventListener("click", changeColor);
