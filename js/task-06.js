a2c3d0f4c04eec485d08248ec6f264457e7b5bcc
Paweł Brzozowski <pawel@brzoza.net>
2023-04-03 10:09:12 +0200

    Task #06

--- js/task-06.js
+++ js/task-06.js
@@ -1,5 +1,9 @@
 const inputEl = document.getElementById("validation-input");
 
-inputEl.addEventListener("focus", (event) => {
+const inputCheck = () => {
+    if (inputEl.ariaValueMax.length === Number(input.dataset.length)) {
+        inputEl.className = "valid";
+    } else inputEl.className = "invalid";
+};
 
-});
+inputEl.addEventListener("focus", inputCheck);
