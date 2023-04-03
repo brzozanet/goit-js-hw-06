bf49f68be473a79ec57ae5c18c78bf46d41492f5
Paweł Brzozowski <pawel@brzoza.net>
2023-04-03 11:24:19 +0200

    Task #08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formEl = document.querySelector(".login-form");
+let userData = {};
 
+const sendForm = (event) => {
+    event.preventDefault();
+    const {elements: {email, password}} = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        alert("Proszę wypełnić wszystkie pola formularza.")
+    } else {
+        userData = {email: `${email.value}`, password: `${password.value}`};
+        console.log(userData);
+        event.currentTarget.reset();
+    };
+};
+
+formEl.addEventListener("submit", sendForm);
